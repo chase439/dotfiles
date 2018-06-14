f7c05f8bcbd9ed8d27e5485977965fd2c07f5f18
Chase Tran <rapidror@gmail.com>
2018-06-14 00:08:08 -0400

    update gem.sh with gpg and source rvm script

--- install/gem.sh
+++ install/gem.sh
@@ -3,10 +3,13 @@ if ! is-macos -o ! is-executable brew; then
   return
 fi
 
+brew install gpg2
+
 \curl -sSL https://get.rvm.io | bash -s stable
 
-# brew install gpg2
-# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 rvm install 1.9.3
 rvm install 2.5
