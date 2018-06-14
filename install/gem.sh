6778185841b9d9d2cad24c774b76edc2a294a29b
Chase Tran <rapidror@gmail.com>
2018-06-14 01:06:32 -0400

    update gem.sh

--- install/gem.sh
+++ install/gem.sh
@@ -7,12 +7,14 @@ brew install gpg2
 
 \curl -sSL https://get.rvm.io | bash -s stable
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+# rvm get head  # upgrade rvm to head of master branch instead of its latest version
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 rvm install 1.9.3
-rvm install 2.5
+rvm install 2.4
 rvm use 1.9.3 --default
 
 gem install lunchy
