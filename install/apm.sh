97b6d0f41ecf3851438ba85a213935677c04c72c
Chase Tran <rapidror@gmail.com>
2018-09-03 03:50:04 +0000

    add notes on atom packages

--- install/apm.sh
+++ install/apm.sh
@@ -4,9 +4,15 @@ if ! is-macos -o ! is-executable apm; then
 fi
 
 packages=(
-  # emmet
   atom-html-preview
-  atom-beautify  # format files
+  atom-beautify  # format file's indentation
+  # teletype # Share your workspace with team members & collaborate on code in real time
+  # linter # base linter, tool that analyzes source code to flag programming errors
+  # linter-jshint # js linter
+  # pigments # display colors in project and files.
+  # atom-live-server # launch a http server with live reload capability.
+  # ask-stack # search stackoverflow, Ctrl+Alt+A, questions and answers
+  # emmet # write html code faster
 )
 
 apm install "${packages[@]}"
