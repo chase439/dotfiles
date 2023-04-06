ac93150fc9cc72304995862559252bfe1fff3723
Chase Tran User <chasetran@microsoft.com>
2023-04-06 17:38:28 -0400

    add notes about symlink

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -93,6 +93,6 @@ function prompt {
 }
 
 # source existing files
-Push-Location (Split-Path -parent (Get-Item $profile).Target)
+Push-Location (Split-Path -parent (Get-Item $PROFILE).Target)
 "components","functions","aliases","exports","extra" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
--- windows/symlink.ps1
+++ windows/symlink.ps1
@@ -1,3 +1,8 @@
 # create a symlink to a location where PowerShell sources its profiles
+# WARN: each PowerShell version/type may have a different $PROFILE location. Best to run this on each PS type as admin.
+# SymbolicLink requires admin privileges, Junction doesn't but it only links directory
+
 #New-Item -Path ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName
+# New-Item -Path "$env:OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName -Force
+
 New-Item -Path $PROFILE -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName -Force
